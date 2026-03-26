48eb50c4a8ccbd92ca14c83e628c8904b7915095
Bholendra Singh <bholendra.singh@aajtak.com>
2026-03-26 13:52:21 +0530

    Add comment to cn function in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,6 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
+  //hello word
   return twMerge(clsx(inputs))
 }
